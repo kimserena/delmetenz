858bd462ff4ccdb51772856bf6e830dc23c784bd
serena <serena@delmetenz.com>
2020-02-28 16:23:27 +0900

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,16 @@
+//function scrollShrink() { 
+//    window.addEventListener('scroll', function(e){
+//        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+//            shrinkOn = 150; // 기준점인 y 픽셀. 
+//            
+//        if (distanceY > shrinkOn) { // 줄어든 헤더 버전
+////            $("#search").hide();
+//            $(".navbar-right").css("font-size","12px");
+//            $(".logo-image-desktop").css("height","40px");
+//        } else { // 평소 헤더 버전
+////            $("#search").show();
+//            $(".navbar-right").css("font-size","15px");
+//            $(".logo-image-desktop").css("height","45px");
+//        }
+//    });         
+//}
