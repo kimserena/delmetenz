8a096faddd458df51094bf2be75e32f0ed089c36
kimserena <serena@delmetenz.com>
2020-03-04 18:11:30 +0900

    메뉴 추가 및 수정

--- js/common.js
+++ js/common.js
@@ -14,3 +14,58 @@
 //        }
 //    });         
 //}
+
+//탑버튼
+(function ($) {
+    $(document).ready(function () {
+        $('.m_tops').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 500);
+        })
+    });
+})(jQuery);
+
+
+//스크롤로딩
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+//탭메뉴
+$(document).ready(function(){
+	
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
+
+})
+
+//더보기
+function loadfunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "More"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Less"; 
+    moreText.style.display = "inline";
+  }
+}
